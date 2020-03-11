24ca7e94eb67b9489672b8a17075238fe912d0c3
Milton Vilarino Netto <miltonvilarino@gmail.com>
2020-03-11 20:24:08 -0300

    Styles + unform
    Estilizando a autenticação e utilizando a lib unform para facilitar com os formulários

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
-export default function Singup() {
-  return <div />;
+export default function SingUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Insira seu e-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho Login</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/_layouts/auth/index.js
+++ src/pages/_layouts/auth/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Wrapper } from './styles';
+import { Wrapper, Content } from './styles';
 
 // eslint-disable-next-line react/prop-types
 export default function AuthLayout({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+      <Content>{children}</Content>
+    </Wrapper>
+  );
 }
 
 AuthLayout.protoTypes = {
