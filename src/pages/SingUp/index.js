78dc6c02af1073b3061a85cc9fef600848aeb74e
Milton Vilarino Netto <miltonvilarino@gmail.com>
2020-03-12 02:47:45 -0300

    Config Redux/Redux-saga/Reactotron
    Configurando arquitetura Flux.

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
-
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('o nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail Valido')
+    .required('O e-mail é Obrigatório'),
+  password: Yup.string()
+    .min(6, 'no minimo 6 caracteres')
+    .required('A senha é Obrigatória')
+});
 
 export default function SingUp() {
   function handleSubmit(data) {
@@ -13,7 +23,7 @@ export default function SingUp() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Insira seu e-mail" />
         <Input name="password" type="password" placeholder="Senha" />
